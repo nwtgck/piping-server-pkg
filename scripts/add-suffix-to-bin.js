c401b38ea3f1b7ff28a6ae99f73dba682782b833
Ryo Ota <nwtgck@nwtgck.org>
2021-10-17 00:07:26 +0900

    support arm64

--- scripts/add-suffix-to-bin.js
+++ scripts/add-suffix-to-bin.js
@@ -2,6 +2,12 @@ const fsPromise = require("fs/promises");
 const path = require("path");
 
 
+const suffix = process.argv[2];
+if (suffix === undefined) {
+  process.stderr.write("error: suffix is missing\n");
+  process.exit(1);
+}
+
 (async () => {
   const projectRootDir = path.resolve(".");
   const distDir = path.join(projectRootDir, "dist");
@@ -9,7 +15,7 @@ const path = require("path");
   for (const fileName of fileNames) {
     const extname = path.extname(fileName);
     const basename = path.basename(fileName, extname);
-    const newFileName = `${basename}-x64${extname}`;
+    const newFileName = `${basename}-${suffix}${extname}`;
     const filePath = path.join(distDir, fileName);
     const newFilePath = path.join(distDir, newFileName);
     await fsPromise.rename(filePath, newFilePath);
